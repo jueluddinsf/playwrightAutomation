62d33731d31fb759daabc97e80693d596e0e942a
galinamoeed2024 <Galina.moeedny@gmail.com>
2024-01-25 21:20:13 -0500

    Update learningConflictResolving.spec.js

--- tests/learningConflictResolving.spec.js
+++ tests/learningConflictResolving.spec.js
@@ -1,1 +1,10 @@
 
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  
+  await page.goto('https://www.usps.com/');
+  await expect(page.locator('body')).toContainText('Search or Track Packages');
+  await page.getByRole('link', { name: 'Locations' }).click();
+  await expect(page.locator('body')).toContainText('Find USPS Locations');
+});
