c371dacb9b85a10e0ee4f0f200e546ba6ebca7bd
MdHossainRabu <hossain732392@gmail.com>
2024-01-25 21:18:59 -0500

    code testing

--- tests/learningConflictResolving.spec.js
+++ tests/learningConflictResolving.spec.js
@@ -1,1 +1,13 @@
 
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.only('Verify Application Tittle', async function ({ page }) {
+    //navigate the google website
+    await page.goto("https://www.google.com/")
+    // verify page tittle
+   // const pTittle = await page.title()
+    //console.log("the page tittle is " + pTittle)
+    await expect(page).toHaveTitle("Google")
+
+});
