cd32e7e38def57ad0c12ffe8f18690409b178b95
arefinmarjana <arefinmarjana1@gmail.com>
2024-01-25 21:25:03 -0500

    new edit

--- tests/learningConflictResolving.spec.js
+++ tests/learningConflictResolving.spec.js
@@ -13,3 +13,13 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Logout' }).click();
   await expect(page.locator('[data-test="login-button"]')).toContainText('Login');
 });
+
+
+import { test, expect } from '@playwright/test';
+
+test('Verify Customer Payment and Claim Center Page Content', async ({ page }) => {
+  await page.goto('https://insurancewebsitedemo.com/customer-support/payments-and-claims');
+  await expect(page.locator('h1')).toContainText('Customer Payment and Claim Center');
+  await expect(page.getByText('Below are links that you can')).toBeVisible();
+  await page.getByText('Below are links that you can').click();
+});
