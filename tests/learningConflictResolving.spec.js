63b134b8a91da77fc93398db94b87cf479973469
lahulnahian <nahian.grantho84@gmail.com>
2024-01-25 21:31:07 -0500

    adding a test file in master

--- tests/learningConflictResolving.spec.js
+++ tests/learningConflictResolving.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('verify icc home page ranging feature', async ({ page }) => {
+test('verify icc home page ranging feature11', async ({ page }) => {
   await page.goto('https://www.icc-cricket.com/');
   await page.goto('https://www.icc-cricket.com/rankings');
   await expect(page.locator('h1')).toContainText('ICC World Rankings');
