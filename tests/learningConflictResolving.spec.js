8afa7a6fdeae204b846bd864798be827e93ea314
Juel Uddin <jueluddin@gmail.com>
2024-01-25 21:10:27 -0500

    Update learningConflictResolving.spec.js

--- tests/learningConflictResolving.spec.js
+++ tests/learningConflictResolving.spec.js
@@ -1,15 +1,1 @@
-import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('#item_4_title_link')).toContainText('Sauce Labs Backpack');
-  await expect(page.locator('#item_0_title_link')).toContainText('Sauce Labs Bike Light');
-  await page.getByRole('button', { name: 'Open Menu' }).click();
-  await page.getByRole('link', { name: 'Logout' }).click();
-  await expect(page.locator('[data-test="login-button"]')).toContainText('Login');
-});
