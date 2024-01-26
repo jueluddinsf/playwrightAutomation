56b5dcc6e07cc65023bffa20241f56d8e38febc9
frna323 <frna323@gmail.com>
2024-01-25 21:42:02 -0500

    resolved conflict updated test

--- tests/learningConflictResolving.spec.js
+++ tests/learningConflictResolving.spec.js
@@ -1,22 +1,22 @@
-
+import { test, expect } from '@playwright/test';
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  
-  await page.goto('https://www.usps.com/');
-  await expect(page.locator('body')).toContainText('Search or Track Packages');
-  await page.getByRole('link', { name: 'Locations' }).click();
-  await expect(page.locator('body')).toContainText('Find USPS Locations');
-import { test, expect } from '@playwright/test';
+test('has title', async({ page }) => {
+
+    await page.goto('https://www.usps.com/');
+    await expect(page.locator('body')).toContainText('Search or Track Packages');
+    await page.getByRole('link', { name: 'Locations' }).click();
+    await expect(page.locator('body')).toContainText('Find USPS Locations');
+});
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('#item_4_title_link')).toContainText('Sauce Labs Backpack');
-  await page.getByRole('button', { name: 'Open Menu' }).click();
-  await page.getByRole('link', { name: 'Logout' }).click();
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.locator('#item_4_title_link')).toContainText('Sauce Labs Backpack');
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
 });
