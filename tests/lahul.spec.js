9e9d0378d8a72bfc2ce61661c04ecee46f165203
lahulnahian <nahian.grantho84@gmail.com>
2024-01-30 16:28:57 -0500

    adding new test cases for icc event page feature

--- tests/lahul.spec.js
+++ tests/lahul.spec.js
@@ -14,4 +14,23 @@ test('verify user able to click icc ranking tab and ranking faqs)', async ({ pag
   await expect(page.getByLabel('player rankings faqs')).toBeVisible();
   await page.getByLabel('player rankings faqs').click();
   await page.getByRole('link', { name: 'Header Logo' }).click();
+
+
+
+  test('verify user able to use icc events page feature ', async ({ page }) => {
+    await page.goto('https://www.icc-cricket.com/');
+    await page.getByRole('link', { name: 'Event Page' }).click();
+    await expect(page.locator('#main')).toContainText('India continue to dominate; Pakistan, West Indies also start Super Six phase with wins');
+    await page.getByRole('link', { name: 'Musheer Khan POTM Highlights' }).first().click();
+    await page.getByRole('link', { name: 'The irresistible fanbase of' }).first().click();
+    await page.getByRole('link', { name: 'Sri Lanka v West Indies |' }).first().click();
+    await page.getByRole('link', { name: 'Fixtures confirmed for Super Six stage of U19 Men’s World Cup 2024 U19 Cricket' }).click();
+    await expect(page.getByLabel('ICC Home')).toBeVisible();
+    await page.getByLabel('ICC Home').click();
+    
+  });
+
+
+
+  
 });
