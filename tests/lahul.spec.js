f810c91f8f9709208bc65be75ee580378c313fa0
lahulnahian <nahian.grantho84@gmail.com>
2024-01-30 17:08:47 -0500

    adding test case for icc awards tab feature

--- tests/lahul.spec.js
+++ tests/lahul.spec.js
@@ -27,6 +27,19 @@ test('verify user able to click icc ranking tab and ranking faqs)', async ({ pag
     await page.getByRole('link', { name: 'Fixtures confirmed for Super Six stage of U19 Men’s World Cup 2024 U19 Cricket' }).click();
     await expect(page.getByLabel('ICC Home')).toBeVisible();
     await page.getByLabel('ICC Home').click();
+
+    test('test', async ({ page }) => {
+      await page.goto('https://www.icc-cricket.com/');
+      await page.getByLabel('Awards', { exact: true }).click();
+      await expect(page.locator('.bg-grey-light > div > .inline-block').first()).toBeVisible();
+      await page.locator('.bg-grey-light > div > .inline-block').first().click();
+      await expect(page.locator('#main')).toContainText('ICC Men’s ODI Cricketer of the Year 2023');
+      await expect(page.getByText('ViratKohli').first()).toBeVisible();
+      await expect(page.locator('#main')).toContainText('ViratKohli1Wickets1377Runs12Catches');
+      await page.getByText('ViratKohli1Wickets1377Runs12Catches').first().click();
+      await page.getByRole('link', { name: 'Header Logo' }).click();
+    });
+
     
   });
 
