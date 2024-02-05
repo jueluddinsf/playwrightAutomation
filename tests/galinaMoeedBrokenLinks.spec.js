595ddf90c59999470db827a81ad5aa611dd2fddd
galinamoeed2024 <Galina.moeedny@gmail.com>
2024-02-05 14:12:38 -0500

    test case for broken links

---
+++ tests/galinaMoeedBrokenLinks.spec.js
@@ -0,0 +1,40 @@
+import { test, expect, request } from '@playwright/test';
+//const {test, expect} = require('@playwright/test');
+
+test('Veryfy links on a  HR page', async ({ page }) => {
+    await page.goto('https://www.infosys.com/');
+    await page.locator('.navbar-brand').waitFor(30000);
+    const linkElements = await page.$$('a');
+    console.log(`total links on the page are: ${linkElements.length}`);
+
+    let links = []
+
+    for (const linkElement of linkElements) {
+
+        if (linkElement) {
+            links.push(await linkElement.getAttribute('href'));
+
+        }
+    }
+    console.log(links.lenght);
+    for (const link of links) {
+        if (link && link.includes('http')) {
+            const res = page.goto(link);
+            if (res) {
+                console.log(`${link} worked and the status is ${res.status()}`);
+
+            }
+            else {
+                console.log(`${link} did not work or retuen 200`);
+
+            }
+            await page.waitForTimeout(1000);
+        }
+
+
+
+
+    } 
+
+
+});
