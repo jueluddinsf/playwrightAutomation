607cda2753d387753ae587cbfe4158929d744743
lahulnahian <nahian.grantho84@gmail.com>
2024-02-09 01:12:16 -0500

    modal handling small and large by lahul

--- tests/handelingWithAlert.spec.js
+++ tests/handelingWithAlert.spec.js
@@ -1,6 +1,18 @@
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
+
+test.describe('Handling alerts/modal in web application', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://demoqa.com/alerts');
+    await page.goto('https://demoqa.com/modal-dialogs')
+    await expect(page).toHaveTitle(/DEMOQA/);
+
+  });
+});
+
+
+// "Alert Handling "
+test('verify alert handling', async ({ page }) => {
   await page.goto('https://demoqa.com/alerts');
 
   await expect(page).toHaveTitle(/DEMOQA/);
@@ -12,16 +24,33 @@ test('has title', async ({ page }) => {
   const firstAlertBtn = await page.locator('#alertButton');
   //firstAlertBtn.waitFor();
   await firstAlertBtn.click();
-
   //await page.waitForTimeout(30000);
-
-
-
-
-
- 
-
 
 });
 
+  
+  // "Small-Modal Handling"
 
+  test('verify small-modal handling', async ({ page }) => {
+    await page.on('dialog', async dialog => {
+    const modal = await page.locator('#showSmallModal');
+    //firstAlertBtn.waitFor();
+    await modal.click();
+    await expect(page.locator('.modal-body')).toHaveText('This is a small modal. It has very less content');
+    await page.locator('#closeSmallModal').click();
+
+    });
+});
+
+
+    // "LARGE-MODAL Handling"
+test('verify large-modal handling', async ({ page }) => {
+  await page.on('dialog', async dialog => {
+  const modal = await page.locator('#showLargeModal');
+  //firstAlertBtn.waitFor();
+  await modal.click();
+  await expect(page.locator('.modal-body')).toHaveText('Lorem Ipsum is simply dummy text of the printing and typesetting industry.');
+  await page.locator('#closeLargeModal').click();
+
+ });
+});
